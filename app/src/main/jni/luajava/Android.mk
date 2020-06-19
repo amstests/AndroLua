d5b96940b1cbf477b97ab0e575f0cd6d51f3a43e
Ildar Mulyukov <ildar@altlinux.ru>
2020-06-19 15:44:10 +0600

    push out the LuaJava sources to submodule

--- app/src/main/jni/luajava/Android.mk
+++ app/src/main/jni/luajava/Android.mk
@@ -3,8 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../app/src/main/jni/lua/lua
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/c
 LOCAL_MODULE     := luajava
-LOCAL_SRC_FILES  := luajava.c
+LOCAL_SRC_FILES  := c/luajava.c
 LOCAL_STATIC_LIBRARIES := liblua
 # LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 
